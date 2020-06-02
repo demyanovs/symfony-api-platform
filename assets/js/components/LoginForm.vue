9c18d69cc7d7b591be2bda56311747ecf42d5bfa
Vyacheslav Demyanov <demyanovs@gmail.com>
2020-06-02 23:47:11 +0300

    Login with json_login

--- assets/js/components/LoginForm.vue
+++ assets/js/components/LoginForm.vue
@@ -39,7 +39,6 @@
                 this.isLoading = true;
                 this.error = '';
 
-                /*
                 axios
                     .post('/login', {
                         email: this.email,
@@ -52,11 +51,10 @@
                         //this.email = '';
                         //this.password = '';
                     }).catch(error => {
-                        console.log(error.response.data);
-                    }).finally(() => {
-                        this.isLoading = false;
-                    })
-                */
+                    console.log(error.response.data);
+                }).finally(() => {
+                    this.isLoading = false;
+                })
             },
         },
     }
